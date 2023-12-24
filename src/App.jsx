61a064065637fef45faf93de416fb118eeb3978b
Breno Alves <breno_ja@hotmail.com>
2023-12-24 20:12:11 -0300

    deleted items that will be modified and installed tailwindcss to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-
-import Home from "./pages/Home";
-import Movie from "./pages/Movie";
-import Search from "./pages/Search";
-
-import styles from "./App.module.css";
-
-const App = () => {
-  return (
-    <div className={styles.app}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="movie/:id" element={<Movie />} />
-          <Route path="search" element={<Search />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+export const App = () => {
+   return <div>Hello World</div>;
 };
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./App";
+import { App } from './app.jsx';
 
-import styles from "./main.module.css";
+import './main.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
